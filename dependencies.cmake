34a1f14eef85ae300a53603ac726da81201c46e8
Tim Henning <tim@luminosuslight.de>
2025-06-23 09:57:19 +0200

    Add dependencies.cmake to be included in main CMake file

---
+++ dependencies.cmake
@@ -0,0 +1,39 @@
+# --- Source Files ---
+set(PROJECT_HEADERS
+    ${PROJECT_HEADERS}
+    qsyncable/qsdiffrunner.h
+    qsyncable/qspatch.h
+    qsyncable/qspatchable.h
+    qsyncable/qslistmodel.h
+    qsyncable/qsuuid.h
+    qsyncable/priv/qsdiffrunneralgo_p.h
+    qsyncable/priv/qstree.h
+    qsyncable/priv/qstreenode.h
+    qsyncable/qsjsonlistmodel.h
+    qsyncable/qsyncablefunctions.h
+    qsyncable/qsyncableqmlwrapper.h
+    qsyncable/priv/qsalgotypes_p.h
+    qsyncable/priv/qsimmutablewrapper_p.h
+    qsyncable/priv/qsfastdiffrunneralgo_p.h
+    qsyncable/qsfastdiffrunner.h
+)
+
+set(PROJECT_SOURCES
+    ${PROJECT_SOURCES}
+    qsyncable/qsdiffrunner.cpp
+    qsyncable/qspatch.cpp
+    qsyncable/qslistmodel.cpp
+    qsyncable/qsuuid.cpp
+    qsyncable/qsdiffrunneralgo.cpp
+    qsyncable/qstree.cpp
+    qsyncable/qstreenode.cpp
+    qsyncable/qsjsonlistmodel.cpp
+    qsyncable/qsyncableqmltypes.cpp
+    qsyncable/qsyncablefunctions.cpp
+    qsyncable/qsyncableqmlwrapper.cpp
+)
+
+# --- Resource Files ---
+set(PROJECT_RESOURCES
+    ${PROJECT_RESOURCES}
+)
